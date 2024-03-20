4eb59371c4a274f45bc57af16b050b82c3275a9c
FR13ND-ly <57838113+FR13ND-ly@users.noreply.github.com>
2024-03-20 13:52:05 +0200

    fix requirements

--- backend/settings.py
+++ backend/settings.py
@@ -109,12 +109,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
-
 API_URL = os.getenv("API_URL")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
